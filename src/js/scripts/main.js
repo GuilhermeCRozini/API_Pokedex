e39ba008f6810d40cc65bb31adf78b5b0c4b8182
Guilherme <guicrozini@gmail.com>
2022-08-03 01:35:10 -0300

    Adicionado mensagem de erro para pesquisa inválida

--- src/js/scripts/main.js
+++ src/js/scripts/main.js
@@ -15,87 +15,64 @@ var slide_hero = new Swiper(".slide-hero", {
 
 //*********************************************************//
 
-// como serão vários cards, irei selecionar todos e depois fazer o evento de click somente no que eu cliquei
 const cardPokemon = document.querySelectorAll('.js-open-details-pokemon');
 const btnCloseModal = document.querySelector('.js-close-modal-details-pokemon');
 const countPokemons = document.getElementById('js-count-pokemons');
 
-
-// mapeando tudo, porque o querySelector retorna para mim um vetor com todos os itens que existem da classe
-// percorrendo cada item cada item que existe e fazer o evento de click
-// O primeiro parâmetro será o card que é o item que irei clicar
 cardPokemon.forEach(card => {
   card.addEventListener('click', openDetailsPokemon);
 })
 
-//Se o JS vier na página e encontrar esse elemento, ele vai entrar no if e executar essa função, mas se entrar na página e não encontrar o btnCloseModal, vai dar false, ou seja, vai entrar no if e sair direto sem exibir erro
 if(btnCloseModal) {
   btnCloseModal.addEventListener('click', closeDetailsPokemon);
 }
-
 
 //Referente à exibição da lista dos tipos de Pokémon para o Mobile
 
 const btnDropdownSelect = document.querySelector('.js-open-select-custom');
 
 btnDropdownSelect.addEventListener('click', () => {
-//Pegando o pai do elemento btnDropdownSelect que é o select-custom 
-//toggle - se estiver fechado ele abre e se estiver aberto ele fecha
   btnDropdownSelect.parentElement.classList.toggle('active');
 })
 
 const areaPokemons = document.getElementById('js-list-pokemons');
 
 function primeiraLetraMaiuscula(string) {
-//O parâmetro string é o nome do Pokémon que está sendo passado
-//Colocando a primeira letra do Pokémon em maiúsculo
-//O charAt pega a string, transforma em um vetor, e irá pegar cada letra
-//Definindo a posição 0 para pegar a primeira letra e o toUpperCase está a deixando maiúscula
-//string.slice(1) -> Está pegando toda a minha string sem a primeira posição
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-//Função criando o HTML pelo JS
 function createCardPokemon(code, type, nome, imagePok) {
-//Criando o botão que é o próprio card
+
   let card = document.createElement('button');
+
 //Adicionando três classes ao card
-//O tipo será preenchido de acordo com o Pokémon que está vindo
   card.classList = `card-pokemon js-open-details-pokemon ${type}`;
   card.setAttribute('code-pokemon', code);
-//Onde eu vou adicionar esse card que estou criando
-//Indicando areaPokemon e o que eu criarei dentro dele, que é o card que acabei de criar
   areaPokemons.appendChild(card);
 
-//Criando a div imagem
+//Div imagem
   let image = document.createElement('div');
   image.classList = 'image';
   card.appendChild(image);
 
-//Imagem que tem dentro da div
-//Criando o elemento img
+//Elemento img
   let imageSrc = document.createElement('img')
   imageSrc.className = 'thumb-img';
-//Definindo um atributo src para img, e inserindo o imagePok no atributo
   imageSrc.setAttribute('src', imagePok);
-//Inserindo o imageSrc dentro da div image
   image.appendChild(imageSrc);
 
-//Criando a div info
+//Div info
   let infoCardPokemon = document.createElement('div');
   infoCardPokemon.classList = 'info';
   card.appendChild(infoCardPokemon);
 
-//Criando a div text
+//Div text
   let infoTextPokemon = document.createElement('div');
   infoTextPokemon.classList = 'text';
   infoCardPokemon.appendChild(infoTextPokemon);
 
-//Criando o span code
+//Span code
   let codePokemon = document.createElement('span');
-//Valor do código do Pokémon
-//If ternário é um if só que de forma reduzida
-//Se o meu código for menor do que 10 (Porque precisa preencher de 01 até 9), eu quero que o meu texContent preencha com #00${code}, se for falso 
   codePokemon.textContent = (code < 10) ? `#00${code}` : (code < 100) ? `#0${code}` : `#${code}`;
   infoTextPokemon.appendChild(codePokemon);
 
@@ -109,15 +86,12 @@ function createCardPokemon(code, type, nome, imagePok) {
   areaIcon.classList = 'icon';
   infoCardPokemon.appendChild(areaIcon);
 
-//Adicionando a imagem do tipo
+//Imagem do tipo
   let imgType = document.createElement('img');
   imgType.setAttribute('src', `src/img/icon-types/${type}.svg`)
   areaIcon.appendChild(imgType);
 }
 
-
-//Função para realizar a listagem dos Pokémons
-//O primeiro parâmetro será a url da API que eu irei chamar
 function listingPokemons(urlApi) {
     axios({
       method:'GET',
@@ -126,46 +100,30 @@ function listingPokemons(urlApi) {
     .then((response) => {
       //console.log(response)
 
-  //Deconstruction do JS
-  //Pegando apenas o que eu quero
       const { results, next, count } = response.data;     
-  //Carrega quantos Pokémons já existem no total
       countPokemons.innerText = count;
 
-  //Agora é preciso percorrer o Result, para buscar todos os Pokémons da resposta que a API trouxe, visando pegar a url para fazer uma nova requisição trazendo os detalhes do Pokémon como a imagem, nome, código, tipo, etc.
-  //forEach porque estará percorrendo, Pokémon que é o que cada percorrida irá retornar para mim
       results.forEach(pokemon => {
-  //Buscando todos os detalhes de cada Pokémon que veio nessa primeira listagem.
         let urlApiDetails = pokemon.url;
         //console.log(urlApiDetails);
 
-  //Chamando outra API para trazer os detalhes
         axios({
           method: 'GET',
-    //Ao entrar aqui, vai realizar uma chamada à API de cada detalhe do Pokémon
           url: `${urlApiDetails}`
         })
         .then(response => {
           //console.log(response.data);
 
-    //Realizando outra desestruturação dentro desse response
-    //Esses são os dados que serão mostrados no card principal
           const { name, id, sprites, types } = response.data;
 
-    //Criando um objeto chamado infoCard para organizer meu código, facilitando a leitura
           const infoCard = {
             nome: name,
             code: id,
-    //Vai trazer o caminho da imagem de cada Pokémon da API
             imagePok: sprites.other.dream_world.front_default,
-    //Pegando types na posição 0, para sempre pegar o primeiro tipo, porque alguns Pokémons possuem mais de um tipo
             type: types[0].type.name
           }
 
-    //Acessando o infoCard
          createCardPokemon(infoCard.code, infoCard.type, infoCard.nome, infoCard.imagePok); 
-
-    //Após criado todos os Pokémons, e fazer a listagem acima, irei mapear todos eles para adicionar o evento de clique para abrir os detalhes do Pokémon
           const cardPokemon = document.querySelectorAll('.js-open-details-pokemon');
 
           cardPokemon.forEach(card => {
@@ -176,23 +134,11 @@ function listingPokemons(urlApi) {
     })
   })
 }
-
-//Chamando a função
-//Quando carregar a página, irei executar primeiramente a função para que ela traga os 9 primeiros Pokémons
   listingPokemons('https://pokeapi.co/api/v2/pokemon?limit=9&offset=0')
-/*
-  https://pokeapi.co/api/v2/pokemon?limit=9&offet=0
-  Limit -> Mostra apenas 9
-  Offset -> Definido como 0 para que não pule nenhum, se eu colocar por exemplo 100, irá mostrar só a partir dos que vierem depois do 100
-*/
 
-//Função de abrir
-//Essa função deve ficar depois de criar os elementos porque se ficar antes ela não funcionará
 function openDetailsPokemon() {
-  // Adicionando a classe open-modal no meu HTML
-    document.documentElement.classList.add('open-modal');
 
-  //Enquanto o Modal estiver aberto, o scroll ficará desativado
+    document.documentElement.classList.add('open-modal');
     document.documentElement.style.overflow = 'hidden';
 
   //O this pega onde estou clicando
@@ -209,9 +155,7 @@ function openDetailsPokemon() {
     const codePokemonModal =  document.getElementById('js-code-pokemon-modal');
     const heightPokemonModal = document.getElementById('js-height-pokemon');
     const weightPokemonModal = document.getElementById('js-weight-pokemon');
-//    const mainAbilitiesPokemonModal = document.getElementById('js-main-abilities-pokemon');
 
-//Pegando o src da imagem do card e atrelando no src da imagem do modal
     imgPokemonModal.setAttribute('src', imagePokemon.getAttribute('src'));
     modalDetails.setAttribute('type-pokemon-modal', this.classList[2]);
     iconTypePokemonModal.setAttribute('src', iconTypePokemon.getAttribute('src'));
@@ -237,7 +181,6 @@ function openDetailsPokemon() {
         urlType: data.types[0].type.url
       }
 
-//Vou pegar o Array de types, fazer um forEach nele, para cirar a tag no layout e listar
       function listingTypesPokemon() {
         const areaTypesModal = document.getElementById('js-types-pokemon');
 
@@ -261,7 +204,6 @@ function openDetailsPokemon() {
 
         areaWeak.innerHTML = '';
 
-//Percorrendo a API com o tipo que eu tenho para retornar as fraquezas
         axios({
           method: 'GET',
           url: `${infoPokemon.urlType}`
@@ -282,11 +224,7 @@ function openDetailsPokemon() {
       }
 
       heightPokemonModal.textContent = `${infoPokemon.height / 10}m`;
-//Apareceu 69, para converter para 6.9, vou dividir por 10
       weightPokemonModal.textContent = `${infoPokemon.weight / 10}Kg`;
-      //mainAbilitiesPokemonModal.textContent = infoPokemon.mainAbilities;
-
-      //secondAbilityPokemonModal.textContent = `${primeiraLetraMaiuscula(data.abilities[1].ability.name)}`;
 
 //**************************** FUNCIONALIDADE MOSTRAR TODAS AS HABILIDADES ****************************/
 
@@ -337,16 +275,12 @@ function openDetailsPokemon() {
     })
   }
   
-  //Função de fechar
   function closeDetailsPokemon() {
-    // Adicionando a classe open-modal no meu HTML
       document.documentElement.classList.remove('open-modal');
-
-    //  Reativa o scroll da página quando fechar o modal
       document.documentElement.style.overflow = 'auto';
     }
 
-// Script para listar todos os tipo de Pokémon
+// Listando todos os tipo de Pokémon
 
 const areaTypes =  document.getElementById('js-type-area');
 const areaTypesMobile = document.querySelector('.dropdown-select');
@@ -363,7 +297,7 @@ const areaTypesMobile = document.querySelector('.dropdown-select');
         if(index < 18) {
 
           let itemType = document.createElement('li');
-//Inserindo todos os li no areaTypes
+
           areaTypes.appendChild(itemType);
           
           let buttonType = document.createElement('button');
@@ -419,15 +353,11 @@ const areaTypesMobile = document.querySelector('.dropdown-select');
 
 const btnLoadMore = document.getElementById('js-btn-load-more');  
 
-//Toda vez que for clicar no Botão Load More, vamos criar um contador que irá incrementar o número 9 (limit)
-//Quero manter os Pokémons que estavam aparecendo e que os novos que aparecerem ao clicar no botão se junte a eles
-//O contador começará de 10, já que possuo 9 Pokémons já carregados na página
 let countPagination = 10;
 
     function showMorePokemon() {
       listingPokemons(`https://pokeapi.co/api/v2/pokemon?limit=9&offset=${countPagination}`);
 
-//Pegando o meu valor atual e somando 9, por exemplo se o valor do countPagination for 10, na próxia vez que clicar no botão, será 19 e seguirá o loop assim por diante
       countPagination = countPagination + 9;
     }
 
@@ -448,7 +378,7 @@ let countPagination = 10;
 
     const sectionPokemons = document.querySelector('.s-all-info-pokemons');
     
-//Pegando a posição inicial sessão
+//Pegando a posição inicial da sessão
 //Mostra onde se inicia a sessão, nesse caso mostra 706
     const topSection = sectionPokemons.offsetTop;    
 //  console.log(topSection);
@@ -466,8 +396,7 @@ let countPagination = 10;
 
 //Depois que percorreu todos, executa essa linha de comando e só ativa o que eu cliquei    
     this.classList.add('active');
-
-//Antes da minha chamada da API, se encontrar algum número é porque será verdadeiro, então entrará dentro desse if, se for falso vai resetar a minha área, dar um display block no botão btnLoadMore e chamar o listingPokemons, que é de quando carrega a página    
+  
     if(idPokemon) {
       axios({
         method: 'GET',
@@ -497,7 +426,7 @@ let countPagination = 10;
                 imagePok: sprites.other.dream_world.front_default,
                 type: types[0].type.name
               }
-              
+ /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////             
   //Só cria o card se tiver a imagem do Pokémon
               if(infoCard.imagePok) {
                 createCardPokemon(infoCard.code, infoCard.type, infoCard.nome, infoCard.imagePok); 
@@ -512,7 +441,6 @@ let countPagination = 10;
         })
       })
     } else {
-//Lembrando que deve sempre limpar antes e listar depois e não o contrário
     areaPokemons.innerHTML = '';
 
     listingPokemons('https://pokeapi.co/api/v2/pokemon?limit=9&offset=0');
@@ -525,20 +453,15 @@ let countPagination = 10;
 /********************** FUNCIONALIDADE DO SEARCH **********************/
 
 const btnSearch = document.getElementById('js-btn-search');
-//Botão iniciará desativado
   btnSearch.disabled = true;
+
 const inputSearch = document.getElementById('js-input-search');
 
-//Event listener que escuta mudanças no input
   addEventListener('input', () => {
-  //Buscando conteúdo do input
     let conteudo = inputSearch.value;
-    //Validando conteúdo do input
       if (conteudo !== null && conteudo !== '') {
-      //Habilita o botão
         btnSearch.disabled = false;
       } else {
-    //Desabilita o botão se o conteúdo do input estiver em branco
         btnSearch.disabled = true;
       }
   })
@@ -546,16 +469,12 @@ const inputSearch = document.getElementById('js-input-search');
 btnSearch.addEventListener('click', searchPokemon);
 
 inputSearch.addEventListener('keyup', (event) => {
-//Capturando o evento do ENTER (Quando clica no enter do teclado para buscar)
-//Verificação se o botão que eu cliquei é o ENTER
-//Só funcionará se tiver algo digitado dentro do input
   if(event.code === 'Enter' && inputSearch.value.length > 0) {
     searchPokemon();
   }
 })
 
   function searchPokemon() {
-//Pegando o que foi digitado no campo
     let valueInput = inputSearch.value.toLowerCase();
     const typeFilter = document.querySelectorAll('.type-filter');
 
@@ -572,7 +491,6 @@ inputSearch.addEventListener('keyup', (event) => {
 
       areaPokemons.innerHTML = '';
       btnLoadMore.style.display = 'none';
-//O contador de Pokémons mostrará 1, porque OBVIAMENTE não existe mais de um Pokémon com o mesmo nome e código
       countPokemons.textContent = 1;
 
         const { name, id, sprites, types } = response.data;
@@ -597,12 +515,30 @@ inputSearch.addEventListener('keyup', (event) => {
 //Função com tratamento de erro do Axios
 //Caso contenha algum erro, irá cair dentro do catch
     .catch((error) => {
-      if(error.response) {
+      if(error.response) {  
+
         areaPokemons.innerHTML = '';
         btnLoadMore.style.display = 'none';
-//Recebendo 0 porque não encontrou nada
         countPokemons.textContent = 0;
-        alert('Não foi encontrado nenhum resultado com esta pesquisa!');
+
+        const divConteudoErro = document.createElement('div');
+        divConteudoErro.className = 'conteudo-erro';
+        areaPokemons.appendChild(divConteudoErro);
+
+        const spanErro = document.createElement('span');
+        divConteudoErro.appendChild(spanErro);
+
+        const textoErro = document.createElement('h2');
+        textoErro.textContent = 'Não foi encontrado nenhum resultado com esta pesquisa!';
+        textoErro.className = 'text-erro';
+        spanErro.appendChild(textoErro);
+
+        const imgErro = document.createElement('img');
+        imgErro.setAttribute('src', 'src/img/sad-pikachu.png');
+        imgErro.className = 'imgErro';
+        spanErro.appendChild(imgErro);
+
+        //alert('Não foi encontrado nenhum resultado com esta pesquisa!');
       }
     })    
   }
